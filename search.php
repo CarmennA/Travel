f990c76967cf8de87038d301f79f25901b7df8c3
Ivaylo Kirilov <ivailokirilov93@gmail.com>
2017-02-05 20:28:20 +0200

    Added search

---
+++ search.php
@@ -0,0 +1,39 @@
+<?php
+  include 'restConsumer.php';
+  
+  $result = CallAPI("GET", "https://restcountries.eu/rest/v1/alpha/co");
+  echo $result["capital"];
+?>
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+	<link rel="stylesheet" type="text/css" href="css/search_styles.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<script src="js/jquery-1.11.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h2>Custom search field</h2>
+				<div id="custom-search-input">
+					<div class="input-group col-md-12">
+						<input type="text" class="form-control input-lg" placeholder="Buscar" />
+						<span class="input-group-btn">
+							<button class="btn btn-info btn-lg" type="button">
+								<i class="glyphicon glyphicon-search"></i>
+							</button>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
