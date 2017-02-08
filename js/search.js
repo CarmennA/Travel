edde9145a00209ebd7839d4b37b84a5adf6cac64
Ivaylo Kirilov <ivailokirilov93@gmail.com>
2017-02-09 01:52:43 +0200

    Added autocomplete.
    (should fix bug with hover)

--- js/search.js
+++ js/search.js
@@ -1,8 +1,75 @@
-(function() {
+var AutocompleteSearch = (function() {
+
+    var countries = [];
+    var selectedCode = '';
+
     function loadData(data) {
-        var el = document.getElementById('test');
-        el.textContent = data;
+        countries = JSON.parse(data);
     }
 
-    httpGetAsync("http://localhost:8012/project/Travel/api.php?action=get_all_countries", loadData);
+    function showDropdown() {
+        document.getElementById("myDropdown").classList.add("show");
+    }
+
+    function hideDropdown() {
+        document.getElementById("myDropdown").classList.remove("show");
+    }
+
+    function updateList(text) {
+        if (text === undefined || text === null || text === '') {
+            hideDropdown();
+            return;
+        }
+
+        var filtered = countries.filter(function(item) { return item.name.toLowerCase().includes(text); });
+        var dropdown = document.getElementById("myDropdown");
+        dropdown.innerHTML = "";
+
+        if (filtered.length === 0) {
+            var spanTag = document.createElement('span');
+            spanTag.innerHTML = "No results";
+            dropdown.appendChild(spanTag);
+        } else {
+            filtered.forEach(function(element) {
+                var spanTag = document.createElement('span');
+                spanTag.setAttribute('onclick', "AutocompleteSearch.selectCountry('" + element.name + "', '" + element.code + "');");
+                spanTag.innerHTML = element.name;
+                dropdown.appendChild(spanTag);
+            }, this);
+        }
+
+        showDropdown();
+    }
+
+    function selectCountry(country, code) {
+        selectedCode = code;
+        var searchField = document.getElementById("searchField");
+        searchField.value = country;
+    }
+
+    function search() {
+        location.href = "http://localhost:8012/project/Travel/popular.php?country=" + selectedCode;
+    }
+
+    return {
+        loadData: loadData,
+        showDropdown: showDropdown,
+        hideDropdown: hideDropdown,
+        updateList: updateList,
+        selectCountry: selectCountry,
+        search: search
+    }
+}());
+
+(function() {
+
+    // Close the dropdown menu if the user clicks outside of it
+    // window.onclick = function(event) {
+    //     var dropdown = document.getElementById("myDropdown")
+    //     if (dropdown.classList.contains('show')) {
+    //         dropdown.classList.remove('show');
+    //     }
+    // }
+
+    httpGetAsync("http://localhost:8012/project/Travel/api.php?action=get_all_countries", AutocompleteSearch.loadData);
 })();
